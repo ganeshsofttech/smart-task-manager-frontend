f4bc95dd2921fe6c0160dcd47693e1c2c2bd067d
Ganesh More <ganesh.more3@ibm.com>
2026-07-14 11:13:56 +0530

    Added server URL

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://3.109.184.3:3000',
         changeOrigin: true
       }
     }
